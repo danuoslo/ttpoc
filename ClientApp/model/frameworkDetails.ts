a6275dc9c90b6c8d5ea5ea47156a652974390f23
annapurnagorinta <annapurna.go@oslosolutions.com>
2017-05-08 12:18:05 -0700

    Adding Framework Markup Detail model

---
+++ ClientApp/model/frameworkDetails.ts
@@ -0,0 +1,34 @@
+import {Status} from '../model/statusdropdown';
+
+export class FrameworkMarkupDetails
+{
+   Id : number;
+    ISOControl: string;
+    Description:string;
+    MSControl: string;
+    NecessaryCustomerAction:string;
+    AdditionalGuidance:string;
+    AssignedTo:string;
+    TestPlanFindingNotes:string;
+    status: Array<Status>
+    ImplementationDate: string;
+    TestResult: string;    
+   
+
+    constructor() {
+      this.Id = 0;
+       this.ISOControl="";
+       this.Description="";
+       this.MSControl="";
+       this.NecessaryCustomerAction="";
+       this.AdditionalGuidance="";
+       this.AssignedTo="";       
+       this.TestPlanFindingNotes="";
+       this.ImplementationDate="";
+       this.status=[];
+       this.TestResult="";
+  }
+}
+
+
+
