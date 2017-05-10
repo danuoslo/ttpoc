2da36190d0201282a485e077bcae7478c6b31396
annapurnagorinta <annapurna.go@oslosolutions.com>
2017-05-10 12:46:48 -0700

    Changed data-grid columns

--- ClientApp/components/BindFramework.tsx
+++ ClientApp/components/BindFramework.tsx
@@ -10,41 +10,49 @@ interface BindingProperties {
     onChange:Function;
 }
 
-
 export class BindComponent extends React.Component<BindingProperties, BindComponentState>{
 
   render(){
-    return(  
-      
-<div  data-grid="col-12" className="data-gridboarder">
-     <div data-grid="col-1">{this.props.member.ISOControl}</div>
-     <div data-grid="col-1">{this.props.member.MSControl}  </div>
-     <div data-grid="col-2"> {this.props.member.NecessaryCustomerAction}  </div>
-     <div data-grid="col-2"> {this.props.member.AdditionalGuidance}</div>
-      <div data-grid="col-1">  
-        <button className="c-button" aria-describedby="topFlyoutExample">{ this.props.member.AssignedTo } </button>
-            <div className="c-flyout" id="topFlyoutExample" role="tooltip" data-js-flyout-placement="top" data-js-flyout-dismissible="true" aria-hidden="true">
-              <button>Reassing Task</button>
-              <button>Unassign Task</button>
+    return(        
+       <div data-grid="col-12" className="data-gridboarder">
+            <div data-grid="col-6">
+                <div data-grid="col-6">
+                  <div data-grid="col-3">{this.props.member.ISOControl}</div>
+                  <div data-grid="col-9">{this.props.member.Description}</div>   
+                </div>
+                <div data-grid="col-6">   
+                  <div data-grid="col-3">{this.props.member.MSControl}</div> 
+                  <div data-grid="col-9">{this.props.member.NecessaryCustomerAction}</div>       
+                </div>
             </div>
-      </div>
-      <div data-grid="col-2"> 
-        <textarea id="default-textarea-5" className="f-no-resize f-scroll" name="textarea-default" defaultValue={this.props.member.TestPlanFindingNotes}>
-        </textarea>
-      </div>               
-        <div data-grid="col-1" >
-           <select id="border" className="dropdownGrid">
-             <option>Implementation</option>    
-             <option>AlternativeImplementation</option> 
-             <option>Planned</option>
-             <option>Not in scope</option>            
-           </select>
-       </div>
-       <div data-grid="col-1">  
-         <Datepicker />
+            <div data-grid="col-6">
+                <div data-grid="col-6">
+                    <div data-grid="col-4"><button className="c-button" aria-describedby="topFlyoutExample">{ this.props.member.AssignedTo } </button>
+                        <div className="c-flyout" id="topFlyoutExample" role="tooltip" data-js-flyout-placement="top" data-js-flyout-dismissible="true" aria-hidden="true">
+                              <button>Reassing Task</button>
+                              <button>Unassign Task</button>
+                         </div>
+                    </div>
+                    <div data-grid="col-8"> 
+                     <textarea id="default-textarea-5" className="f-no-resize f-scroll" name="textarea-default" defaultValue={this.props.member.TestPlanFindingNotes}>
+                     </textarea>
+                     </div>
+                 </div>     
+                <div data-grid="col-6">
+                     <div data-grid="col-5">
+                        <select id="border" className="dropdownGrid">
+                             <option>Implementation</option>    
+                             <option>AlternativeImplementation</option> 
+                             <option>Planned</option>
+                              <option>Not in scope</option>            
+                          </select>
+                      </div>
+                    <div data-grid="col-3"><Datepicker /></div>
+                    <div data-grid="col-4">{this.props.member.TestResult}</div>  
+                  </div>          
+              </div>
         </div>
-       <div data-grid="col-1"> {this.props.member.TestResult}</div>
-   </div>  
+
       ); 
     }
   }
