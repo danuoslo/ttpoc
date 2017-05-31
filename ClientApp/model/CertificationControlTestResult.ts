5c66b9deae5df7d114e74fe88e2c407f1fe517bf
Ricardo Aldan Garcia (Oslo Solutions LLC) <v-rialda@microsoft.com>
2017-05-31 13:05:11 -0700

    Adding new CertificationControlTestResultStatus

--- ClientApp/model/CertificationControlTestResult.ts
+++ ClientApp/model/CertificationControlTestResult.ts
@@ -1,36 +1,30 @@
-import {LocalizedValue} from './LocalizedValue';
+import { LocalizedValue } from './LocalizedValue';
+import { CertificationControlTestResultStatus } from "./CertificationControlTestResultStatus";
+
+export class CertificationControlTestResult {
+
+    // ID
+    public id: string;
+    // CERTIFICATIONCONTROLID
+    public certificationControlId: string;
+    // WHENTESTED
+    public whenTested: Date;
+    // WHOTESTED
+    public whoTested: string;
+    // TESTPLANDETAILS
+    public testPlanDetails: Array<LocalizedValue>;
+    // FINDINGS
+    public findings: Array<LocalizedValue>;
+    // MANAGEMENTRESPONSE
+    public managementResponse: Array<LocalizedValue> = [];
+    // LOWRISKFINDINGSCOUNT
+    public lowRiskFindingsCount: number;
+    // MEDIUMRISKFINDINGSCOUNT
+    public mediumRiskFindingsCount: number;
+    // HIGHRISKFINDINGSCOUNT
+    public highRiskFindingsCount: number;
+
+    public status: CertificationControlTestResultStatus
+}
 
 
-    // $Classes/Enums/Interfaces(filter)[template][separator]
-    // filter (optional): Matches the name or full name of the current item. * = match any, wrap in [] to match attributes or prefix with : to match interfaces or base classes.
-    // template: The template to repeat for each matched item
-    // separator (optional): A separator template that is placed between all templates e.g. $Properties[public $name: $Type][, ]
-
-    // More info: http://frhagn.github.io/Typewriter/
-
-    
-    export class CertificationControlTestResult {
-         
-		// ID
-        public id: string; 
-		// CERTIFICATIONCONTROLID
-        public certificationControlId: string; 
-		// WHENTESTED
-        public whenTested: Date; 
-		// WHOTESTED
-        public whoTested: string; 
-		// TESTPLANDETAILS
-        public testPlanDetails: Array<LocalizedValue>; 
-		// FINDINGS
-        public findings: Array<LocalizedValue>; 
-		// MANAGEMENTRESPONSE
-        public managementResponse: Array<LocalizedValue> = []; 
-		// LOWRISKFINDINGSCOUNT
-        public lowRiskFindingsCount: number; 
-		// MEDIUMRISKFINDINGSCOUNT
-        public mediumRiskFindingsCount: number; 
-		// HIGHRISKFINDINGSCOUNT
-        public highRiskFindingsCount: number; 
-    }
-
-	
---
+++ ClientApp/model/CertificationControlTestResultStatus.ts
@@ -0,0 +1,3 @@
+export enum CertificationControlTestResultStatus{
+        Passed, FailedLowRisk, FailedHighRisk 
+}
